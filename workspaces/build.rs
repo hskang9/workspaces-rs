60e405cda54a7c175116a099fffc4a723a51d529
Hyungsuk Kang <hskang9@gmail.com>
2022-07-01 20:46:17 +0900

    Update build.rs

--- workspaces/build.rs
+++ workspaces/build.rs
@@ -2,7 +2,7 @@ fn main() {
     let doc_build = cfg!(doc) || std::env::var("DOCS_RS").is_ok();
     if !doc_build && cfg!(feature = "install") {
         // TODO Update commit to stable version once binaries are published correctly
-        near_sandbox_utils::install_with_version("VLAD-test/c90ac67bc16ba9acbd43104eeb1ba73b6600cca9")
+        near_sandbox_utils::install_with_version("master/97c0410de519ecaca369aaee26f0ca5eb9e7de06")
             .expect("Could not install sandbox");
     }
 }
